47db87c862df40bc9885b94307ba3902d94e47e1
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2021-01-08 06:23:47 +0100

    update
    - Added webhook support.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from colorama import init,Fore,Style
 from os import name,system
 from sys import stdout
@@ -6,7 +7,7 @@ from random import choice
 from threading import Thread,Lock,active_count
 from sys import stdout
 from time import sleep
-import json
+from datetime import datetime
 
 class Main:
     def clear(self):
@@ -68,7 +69,7 @@ class Main:
 
     def TitleUpdate(self):
         while True:
-            self.SetTitle(f'[One Man Builds Discord Token Checker Tool] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| RETRIES: {self.retries} ^| THREADS: {active_count()-1}')
+            self.SetTitle(f'[One Man Builds Discord Token Checker Tool] ^| HITS: {self.hits} ^| BADS: {self.bads} ^| RETRIES: {self.retries} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| THREADS: {active_count()-1}')
             sleep(0.1)
 
     def __init__(self):
@@ -87,6 +88,7 @@ class Main:
         self.hits = 0
         self.bads = 0
         self.retries = 0
+        self.webhook_retries = 0
         self.lock = Lock()
 
         config = self.ReadJson('[Data]/configs.json','r')
@@ -94,39 +96,77 @@ class Main:
         self.use_proxy = config['use_proxy']
         self.proxy_type = config['proxy_type']
         self.threads_num = config['threads']
+        self.webhook_enable = config['webhook_enable']
+        self.webhook_url = config['webhook_url']
 
         print('')
+
+    def SendWebhook(self,title,message,icon_url,thumbnail_url,proxy,useragent):
+        try:
+            timestamp = str(datetime.utcnow())
+
+            message_to_send = {"embeds": [{"title": title,"description": message,"color": 65362,"author": {"name": "AUTHOR'S DISCORD SERVER [CLICK HERE]","url": "https://discord.gg/33UzcuY","icon_url": icon_url},"footer": {"text": "MADE BY ONEMANBUILDS","icon_url": icon_url},"thumbnail": {"url": thumbnail_url},"timestamp": timestamp}]}
+            
+            headers = {
+                'User-Agent':useragent,
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                pass
+            elif "You are being rate limited." in response.text:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+            else:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+        except:
+            self.webhook_retries += 1
+            self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
 
 
     def TokenCheck(self,token):
         try:
-            
+            useragent = self.GetRandomUserAgent()
+
             headers = {
-                'User-Agent': self.GetRandomUserAgent(),
+                'User-Agent': useragent,
                 'Authorization':token
             }
 
             response = ''
-
+            proxy = ''
             link = 'https://discord.com/api/v8/users/@me'
 
             if self.use_proxy == 1:
-                response = requests.get(link,headers=headers,proxies=self.GetRandomProxy())
+                proxy = self.GetRandomProxy()
+                response = requests.get(link,headers=headers,proxies=proxy)
             else:
                 response = requests.get(link,headers=headers)
 
-            if 'username' in response.text:
+            if '401: Unauthorized' in response.text:
+                self.PrintText(Fore.WHITE,Fore.RED,'BAD',token)
+                with open('[Data]/[Results]/bads.txt','a',encoding='utf8') as f:
+                    f.write(token+'\n')
+                self.bads += 1
+            elif 'username' in response.text:
                 self.PrintText(Fore.WHITE,Fore.GREEN,'VALID',token)
                 with open('[Data]/[Results]/detailed_hits.txt','a',encoding='utf8') as f:
                     f.write(response.text+'\n')
                 with open('[Data]/[Results]/valids.txt','a',encoding='utf8') as f:
                     f.write(token+'\n')
                 self.hits += 1
-            elif '401: Unauthorized' in response.text:
-                self.PrintText(Fore.WHITE,Fore.RED,'BAD',token)
-                with open('[Data]/[Results]/bads.txt','a',encoding='utf8') as f:
-                    f.write(token+'\n')
-                self.bads += 1
+                if self.webhook_enable == 1:
+                    self.SendWebhook('Discord Token',token,'https://cdn.discordapp.com/attachments/776819723731206164/796935218166497352/onemanbuilds_new_logo_final.png','https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/91_Discord_logo_logos-512.png',proxy,useragent)
             else:
                 self.retries += 1
                 self.TokenCheck(token)
